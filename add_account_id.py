36d6d0634793d720d8e1718013af13bc313d7490
Moataz Medhat ElQadi <Moataz.Mahmoud@monash.edu>
2022-04-04 11:57:13 +1000

    updated add_account_id

--- add_account_id.py
+++ add_account_id.py
@@ -10,17 +10,42 @@ These user added tags will propagate to snaphshots of these instances. When thos
 © Moataz ElQadi, 2022
 """
 import boto3
-this_account_id =  boto3.client('sts').get_caller_identity().get('Account')
-instances = boto3.Session().resource('ec2').instances.all()
-for i in instances:
-    print('checking ',i)
-    if i.tags is not None:    
-        tags =  [ tag for tag in i.tags if tag['Key']=='account_id']
-        if tags !=[]:
-            tag = tags[0]
-            if tag['Value'] ==   this_account_id:
-                continue
-    i.create_tags(Tags = [{'Key':'account_id','Value':this_account_id}])
-    print('Added or created account id %s for instance %s'%(this_account_id,i.id))    
-    print()
-    
+
+def addIdToResources(this_account_id,resources):
+    for r in resources:
+        print('checking ',r)
+        if r.tags is not None:    
+            tags =  [ tag for tag in r.tags if tag['Key']=='account_id']
+            if tags !=[]:
+                tag = tags[0]
+                if tag['Value'] ==   this_account_id:
+                    continue
+        r.create_tags(Tags = [{'Key':'account_id','Value':this_account_id}])        
+        print('Added or created account id %s to %s'%(this_account_id,r.id))    
+        print()
+        break
+
+def tagInstances(this_account_id):
+    print('**Tagging instances**')
+    ec2 = boto3.Session().resource('ec2')
+    instances = ec2.instances.all()
+    addIdToResources(this_account_id,instances)
+
+def tagSnapshots(this_account_id):
+    print('**Tagging snapshots**')
+    ec2 = boto3.Session().resource('ec2')
+    snapshots = ec2.snapshots.filter(OwnerIds = [this_account_id])
+    addIdToResources(this_account_id,snapshots)
+
+def tagVolumes(this_account_id):
+    print('**Tagging volumes**')
+    ec2 = boto3.Session().resource('ec2')
+    volumes = ec2.volumes.all()
+    addIdToResources(this_account_id,volumes)
+
+if __name__ == "__main__":    
+    this_account_id =  boto3.client('sts').get_caller_identity().get('Account')
+    tagInstances(this_account_id)
+    tagSnapshots(this_account_id)
+    tagVolumes(this_account_id)
+
